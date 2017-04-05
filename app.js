72f71f54f2635e00c1a6aea5a325e227786343a1
Lukas Gupta-Leary <lukool95@yahoo.com>
2017-04-05 16:12:59 -0400

    Setup images folder

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ var api = require('./routes/api');
 
 var app = express();
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'images', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'dist')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use('/api', api);
 
